eb3daffd81c3ce90507bb9f37e64a55f26db54dd
DomoFox06 <yarnalld0401@student.faytechcc.edu>
2025-01-15 16:37:08 +0000

    Hello World

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+// CSC 134
+// Hello World
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "This is one of the first codes you are doing" << endl;
+    cout << "Let's learn plenty this semester";
+    return 0;
+}
