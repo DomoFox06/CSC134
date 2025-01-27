19448eda97a56c6485ea5f22c3c181d51fca2685
DomoFox06 <yarnalld0401@student.faytechcc.edu>
2025-01-27 16:01:11 +0000

    apples

---
+++ Module1/M1Lab.cpp
@@ -0,0 +1,18 @@
+// CSC 134
+// M1LAB
+// Fox
+// 1/27/2025
+
+#include <iostream>
+using namespace std;
+int main() {
+// This program will simulate an apple orchard.
+// The owner’s name
+string name = “Fox”
+// number of apples owned
+int apples = 100
+// price per apple
+double pricePerApple = 0.25
+
+cout << “Welcome to “ << name;
+cout << “ ’s apple orchard.” << endl;
