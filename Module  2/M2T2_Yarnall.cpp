676089fef5d1eb11fedda3961fb620cd1d166e2f
DomoFox06 <yarnalld0401@student.faytechcc.edu>
2025-02-03 15:42:02 +0000

    sandwhiches

---
+++ Module  2/M2T2_Yarnall.cpp
@@ -0,0 +1,26 @@
+// CSC 134
+// M2T2
+// Fox Yarnall
+// 2/3/2025
+// Simples sales program thst prints well formated receipts
+
+#include <iostream>
+using namespace std;
+int main() {
+// set up the variable
+double food_price = 5.99;
+string store_name = "Sandy Wich";
+string food_item = "Sandwhich";
+// Doing the order
+cout << "Welcome to the " << store_name << endl;
+cout << "You have a " << food_item << " that comes out to $5.99" << endl;
+
+// Figure out subtotal and total
+
+
+
+cout << "Have a nice day!" << endl;
+
+cout << endl;
+return 0;
+}
