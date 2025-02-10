e9f37e11ccec0970d37a6b731cc2c5914446c7e2
DomoFox06 <yarnalld0401@student.faytechcc.edu>
2025-02-10 21:56:02 +0000

    pizzaGood

--- Module  2/M2HW1Q3.cpp
+++ Module  2/M2HW1Q3.cpp
@@ -9,18 +9,19 @@ using namespace std;
 // getting volume and dimentions
 int main() {
 
-int people, pizza, slices, slicesLeft, totalSlices, dividedSlices;
+int people, pizza, slices, slicesLeft, totalSlices, slicesPerperson;
 
 cout << "Amount of people: " << endl;
 cin >> people;
-cout << "Amount of pizzas: " << endl;
+cout << "Amount of boxes of pizzas: " << endl;
 cin >> pizza;
-slices = 8;
+cout << "Slices Per Pizza: "<< endl;
+cin >> slices;
 
 totalSlices = pizza * slices;
 
-dividedSlices = totalSlices / people;
-slicesLeft = totalSlices - dividedSlices;
+slicesPerperson = 3 * people;
+slicesLeft = totalSlices - slicesPerperson;
 
 cout << "Slices left over: " << slicesLeft << endl;
 
