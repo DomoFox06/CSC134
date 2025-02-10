f4949b7e49c9499bd14ec846496ab3dc880e2b3e
DomoFox06 <yarnalld0401@student.faytechcc.edu>
2025-02-10 15:42:50 +0000

    savingnow

--- Module  2/M2Lab1.cpp
+++ Module  2/M2Lab1.cpp
@@ -7,6 +7,27 @@
 using namespace std;
 
 int main() {
-    cout << endl;
+    cout << "Crate Calculation Program" << endl;
+int length, width, hieght;
+int volume;
+
+cout <<"Enter the dimentions of the crate." << endl;
+cout << "Length: ";
+cin >> length;
+cout << "Width: ";
+cin >> width;
+cout << "Hieght: ";
+cin >> hieght;
+
+
+volume = length * width * hieght;
+
+cout << "A crate " << length << "by " << width << "by " << hieght << endl;
+cout << "Is volume " << volume << endl;
+
+
+
+
+
     return 0;
 }
