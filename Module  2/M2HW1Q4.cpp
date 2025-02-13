eb40ac2e48eb954ed8cba2ec5bf5a19f5760a58c
DomoFox06 <yarnalld0401@student.faytechcc.edu>
2025-02-13 21:43:00 +0000

    string

---
+++ Module  2/M2HW1Q4.cpp
@@ -0,0 +1,30 @@
+// M2HW1Q3
+// CSC 134
+// Domonic Yarnall
+// 2/10/2025
+//Gold
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+int main() {
+
+string letsGo, school, team, cheerOne, cheerTwo;
+
+letsGo = "Let's go";
+school = "FTCC";
+team = "Trojans";
+
+cheerOne = letsGo + " " + school;
+cheerTwo = letsGo + " " + team;
+
+cout << cheerOne << endl;
+cout << cheerOne << endl;
+cout << cheerOne << endl;
+cout << cheerTwo << endl;
+
+
+
+    return 0;
+}
