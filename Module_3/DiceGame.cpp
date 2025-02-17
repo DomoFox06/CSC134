54d7b5e8959ae803078a12b2b4902f0a41ebdb67
DomoFox06 <yarnalld0401@student.faytechcc.edu>
2025-02-17 16:37:27 +0000

    almostThereDice

--- Module_3/DiceGame.cpp
+++ Module_3/DiceGame.cpp
@@ -7,38 +7,47 @@
 using namespace std;
 int main() {
 
-int p1, p2;
+int p1, p2, p1total, p2total;
 
-p1 = 0;
-p2 = 0;
+p1total = 0;
+p2total = 0;
 
-cout << "Player 1: " << p1 << endl;
-cout << "Player 2: " << p2 << endl;
+cout << "Player 1: " << p1total << endl;
+cout << "Player 2: " << p2total << endl;
 cout << endl;
+cout << "Enter Player 1 Roll" << endl;
+cin >> p1;
+
+cout << "Enter Player 2 Roll" << endl;
+cin >> p2;
+
+p1total += p1;
+p2total += p2;
+
+cout << "Player 1: " << p1total << endl;
+cout << "Player 2: " << p2total << endl;
+
 cout << "Enter Player 1 Roll" << endl;
 cin >> p1;
 cout << "Enter Player 2 Roll" << endl;
 cin >> p2;
 
-cout << "Player 1: " << p1++ << endl;
-cout << "Player 2: " << p2++ << endl;
+p1total += p1;
+p2total += p2;
 
-cin >> p1;
-cin >> p2;
+cout << "Player 1: " << p1total << endl;
+cout << "Player 2: " << p2total << endl;
 
-cout << "Player 1: " << p1++ << endl;
-cout << "Player 2: " << p2++ << endl;
-
-if (p1 > 100){
+if (p1total > 100){
     cout << "Congradulations Player 1!" ;
 }
-else if (p1 < 100){
+else if (p1total < 100){
     cout << "Try Again";
 }
-if (p2 > 100){
+else if (p2total > 100){
     cout << "Congradulations Player 2!" ;
 }
-else if (p2 < 100){
+else if (p2total < 100){
     cout << "Try Again";
 }
 
