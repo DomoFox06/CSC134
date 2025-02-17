92e20f56122b772bca9af4ffb13e3d7a38b08ccb
DomoFox06 <yarnalld0401@student.faytechcc.edu>
2025-02-17 15:35:44 +0000

    save

---
+++ Module 3 /DiceGame.cpp
@@ -0,0 +1,19 @@
+// CSC 134
+// M1LAB
+// Fox Yarnall
+// 1/29/2025
+
+#include <iostream>
+using namespace std;
+int main() {
+
+int p1, p2;
+
+p1 = 0;
+p2 = 0;
+
+cout << "Player 1: " << p1 << endl;
+cout << "Player 2: " << p2 << endl;
+
+return 0;
+}
