304d395c75484a53b8c8cf5c1a831229728007b9
DomoFox06 <yarnalld0401@student.faytechcc.edu>
2025-02-17 15:52:46 +0000

    saveit

--- Module 3 /DiceGame.cpp
+++ Module 3 /DiceGame.cpp
@@ -15,5 +15,30 @@ p2 = 0;
 cout << "Player 1: " << p1 << endl;
 cout << "Player 2: " << p2 << endl;
 
+cin >> p1;
+cin >> p2;
+
+cout << "Player 1: " << p1++ << endl;
+cout << "Player 2: " << p2++ << endl;
+
+cin >> p1;
+cin >> p2;
+
+cout << "Player 1: " << p1++ << endl;
+cout << "Player 2: " << p2++ << endl;
+
+if (p1 > 300){
+    cout << "Congradulations Player 1!" ;
+}
+else if (p1 < 300){
+    cout << "Try Again";
+}
+if (p2 > 300){
+    cout << "Congradulations Player 2!" ;
+}
+else if (p1 < 300){
+    cout << "Try Again";
+}
+
 return 0;
 }
