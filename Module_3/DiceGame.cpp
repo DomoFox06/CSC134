b93b1d1125dff0322bb64ff60771fc24e374ca8a
DomoFox06 <yarnalld0401@student.faytechcc.edu>
2025-02-17 21:04:23 +0000

    whileAttempt

--- Module_3/DiceGame.cpp
+++ Module_3/DiceGame.cpp
@@ -12,7 +12,8 @@ int p1, p2, p1total, p2total;
 
 p1total = 0;
 p2total = 0;
-
+while (p1total < 50){
+while (p2total < 50){
 cout << "Player 1: " << p1total << endl;
 cout << "Player 2: " << p2total << endl;
 cout << endl;
@@ -25,48 +26,17 @@ cin >> p2;
 p1total += p1;
 p2total += p2;
 
-cout << "Player 1: " << p1total << endl;
-cout << "Player 2: " << p2total << endl;
-
-if (p1total > 50){
-    cout << "Congradulations Player 1!" ;
-    return 0;
+cout << endl;
+    }
 }
-else if (p1total < 50){
-    cout << "Try Again" << endl;
-}
-else if (p2total > 50){
-    cout << "Congradulations Player 2!" ;
-    return 0;
-}
-else if (p2total < 50){
-    cout << "Try Again" << endl;
-}
-
-cout << "Enter Player 1 Roll" << endl;
-cin >> p1;
-cout << "Enter Player 2 Roll" << endl;
-cin >> p2;
-
-p1total += p1;
-p2total += p2;
-
-
-cout << "Player 1: " << p1total << endl;
-cout << "Player 2: " << p2total << endl;
 
 if (p1total > 50){
     cout << "Congradulations Player 1!" ;
 }
-else if (p1total < 50){
-    cout << "Try Again" << endl;
-}
 else if (p2total > 50){
     cout << "Congradulations Player 2!" ;
 }
-else if (p2total < 50){
-    cout << "Try Again" << endl;
-}
+
 
 return 0;
 }
