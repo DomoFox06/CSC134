b2b3dfd65e265d21ee944b1ea200a4b69a4632f8
DomoFox06 <yarnalld0401@student.faytechcc.edu>
2025-02-26 15:50:22 +0000

    gradingLookingGood

---
+++ Module_3/HomeWorkM3/YarnallQ1.cpp
@@ -0,0 +1,22 @@
+// CSC 134
+// Domonic Yarnall
+//HWM3Q1
+// 2/26/2025
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+string yes = "yes";
+string no = "no";
+string feeling;
+
+cout << "Hello! How are you doing? Good or bad?" << endl;
+cout << "(type good or bad)" << endl;
+cout << "I am doing " << feeling << endl;
+cin >> feeling;
+
+
+return 0;
+}
---
+++ Module_3/M3Lab2_yarnall.cpp
@@ -0,0 +1,50 @@
+// CSC 134
+// Domonic Yarnall
+//HWM3Q1
+// 2/26/2025
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+int num_grade;
+string letter_grade;
+
+const int A_PLUS= 100;
+const int A     = 90;
+const int B     = 80;
+const int C     = 70;
+const int D     = 60;
+const int F     = 0;
+
+cout << "Number grade to letter grde connvertion program." << endl;
+cout << "Enter the number grade: " ;
+cin >> num_grade;
+
+if (num_grade >= A_PLUS) {
+    letter_grade = "A+";
+}
+if (num_grade >= A && num_grade < A_PLUS) {
+    letter_grade = "A";
+}
+if (num_grade >= B && num_grade < A){
+letter_grade = "B";
+}
+if (num_grade >= C && num_grade < B){
+    letter_grade = "C";
+}
+if (num_grade >= D && num_grade < C){
+    letter_grade = "D";
+}
+if (num_grade >= F && num_grade < D){
+    letter_grade = "F";
+}
+cout << "The grade " << num_grade << " is a " << letter_grade << endl;
+
+
+
+
+
+return 0;
+}
