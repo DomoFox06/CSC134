40110ead377e7c12d2a703fcf2f16e4bd3975262
DomoFox06 <yarnalld0401@student.faytechcc.edu>
2025-02-26 22:26:59 +0000

    reciept done

--- Module_3/HomeWorkM3/YarnallQ2.cpp
+++ Module_3/HomeWorkM3/YarnallQ2.cpp
@@ -16,25 +16,32 @@ string food_item = "Sandwhich";
 string dining;
 string yes = "yes";
 string no = "no";
-double tax_cost = 0.15;
+double tax_cost = 0.08;
+double tip = 0.15;
+double totalTip;
 // Doing the order
 cout << "Welcome to the " << store_name << endl;
 cout << "You have a " << food_item << endl;
-cout << "Please enter the price of the " << food_item << endl;
+cout << "Please enter the price of the " << food_item << " $";
 cin >> food_price;
-cout << food_item << "costs $" << food_price << endl;
+cout << food_item << " costs $" << food_price << endl;
 
 cout << endl;
 cout << "Will you be dining in with us today?" << endl;
 cout << "Enter yes or no" << endl;
 cin >> dining;
 
+
+
 if (dining == no){
-tax_cost = 0;
+tip = 0;
 }
+cout << endl;
+cout << "Awesome! Here is your reciept!" << endl;
 // Figure out subtotal and total
 double subtotal = food_price * tax_cost;
-double total = subtotal + food_price;
+totalTip = tip * food_price;
+double total = subtotal + food_price + tip;
 cout << fixed << setprecision (2);
 
 cout << "           " << endl;
@@ -42,6 +49,7 @@ cout << "YOUR RECIEPT" << endl;
 cout << "------------" << endl;
 cout << "Subtotal: $" << food_price << endl;
 cout << "Tax:      $" << subtotal << endl;
+cout << "Tip:      $" << totalTip << endl;
 cout << "------------" << endl;
 cout << "Total:    $" << total << endl;
 cout << "           " << endl;
