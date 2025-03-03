58510ef7b07b85e0842a1fc7480cae35db0410e9
DomoFox06 <yarnalld0401@student.faytechcc.edu>
2025-03-03 16:30:12 +0000

    did adventure

--- Module_3/HomeWorkM3/YarnallQ3.cpp
+++ Module_3/HomeWorkM3/YarnallQ3.cpp
@@ -9,18 +9,18 @@ using namespace std;
 int main() {
 
 string fd;
-string F = "Fire";
-string T = "tourniquet";
+string F = "F";
+string T = "T";
 string choice;
-string C = "Opened the tomb";
-string L = "Lost your leg";
+string C = "C";
+string L = "L";
 
 cout << "You're in the jungle and have reached the tomb which holds the crystal that will make you immortal and cure the deadly snake bite you have on your ankle." << endl;
 cout << "You have 5 minutes before the poison kills you. What do you do? " << endl;
 cout << "Open the tomb and use the crystal, or cut off your leg?" << endl;
 cout << "(type C to get the crystal or L to cut off your leg)" << endl;
 cin >> choice;
-cout << "You " << choice << endl;
+cout << "You chose " << choice << endl;
 
 
 if (choice == C){
@@ -43,6 +43,7 @@ else if (choice == L){
         cout << "You died a slow death" << endl;
         cout << "Game Over!" << endl;
         return 0;
+        
     }
     else if (fd == T){
         cout << "You did it! You are alive and help is on the way!" << endl;
@@ -50,12 +51,12 @@ else if (choice == L){
     }
     else {
         cout << "I am afraid I do not know what that means" << endl;
-        cout << "I only understand the words yes or no" << endl;
+        cout << "I only understand the choices f or t" << endl;
     }
 }
 else {
     cout << "I am afraid I do not know what that means" << endl;
-    cout << "I only understand the feelings good or bad" << endl;
+    cout << "I only understand the choices c or l" << endl;
 }
 
 return 0;
