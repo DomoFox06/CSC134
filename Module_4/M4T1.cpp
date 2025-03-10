a1c58cefc83c01c3f813a077028b69582f9380e1
DomoFox06 <yarnalld0401@student.faytechcc.edu>
2025-03-10 14:21:47 +0000

    m4t1

---
+++ Module_4/M4T1.cpp
@@ -0,0 +1,24 @@
+//Domonic Yarnall
+//CSC-134
+//M4T1
+//3/10/25
+
+
+#include <iostream>
+using namespace std;
+
+int main (){
+
+int num = 1;
+while(num <= 5){
+    cout << "Hello number " << num << endl;
+    num ++;
+
+}
+cout << "Done!" << endl;
+
+
+
+
+    return 0;
+}
