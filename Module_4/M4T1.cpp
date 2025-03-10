c351f62b858a3f47f0026c959bdeffc5cc9e229c
DomoFox06 <yarnalld0401@student.faytechcc.edu>
2025-03-10 14:50:39 +0000

    save

--- Module_4/M4T1.cpp
+++ Module_4/M4T1.cpp
@@ -16,11 +16,11 @@ while(num <= 5){
 
 }
 cout << "Done!" << endl;
-/*
-for (int i = 1; i <= 10;; i++){
+
+for (int i = 1; i <= 10; i++){
     cout << "Happy Mar" << i << endl;
 }
-*/
+
 cout << "Table of Square" << endl;
 cout << "---------------" << endl;
 int mun = 1;
@@ -29,6 +29,27 @@ while(mun <= 10){
     sq = mun * mun;
     cout << mun << "\t" << sq << endl;
     mun = mun + 1;
+
+    bool is_valid= false;
+    int choice;
+    while (is_valid == false){
+    cout << "Press 1 or 2 to finish." << endl;
+    cin >> choice;
+    if (choice == 1){
+        cout << "You chose 1." << endl;
+        is_valid = true;
+
+    }
+    else if (choice == 2){
+        cout << "You chose 2." << endl;
+        is_valid = true;
+    }
+    else {
+        cout << "Sorry, not valid. Try again." << endl;
+    }
+   
 }
+cout << "Goodbye" << endl;
     return 0;
+}
 }
