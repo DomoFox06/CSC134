42d1c58a9d2f6fa076e4b91a95543d3fc486123f
DomoFox06 <yarnalld0401@student.faytechcc.edu>
2025-03-17 15:11:12 +0000

    save

--- Module_4/M4Lab1.cpp
+++ Module_4/M4Lab1.cpp
@@ -12,14 +12,20 @@ int main(){
 //The emojis overlap with each other, specifically the heart emoji hides behind the rice emoji
 string pixel = "🦊❤️🍚";
 cout << pixel << endl;
-
-
+ int W;
+ cout << "Enter Width: ";
+cin >> W;
+int H;
+cout << endl << "Enter Hight: ";
+cin >> H;
+cout << endl;
 int count = 0;
 while (count < 10){
     cout << pixel;
     count++;
 
 }
+
 cout << endl;
 count = 0;
 do{
@@ -28,11 +34,23 @@ do{
 
 }while(count < 5);
 cout << endl;
+cout << pixel << "printed sideways" << endl;
 for (int i = 0; i < 10; i++){
     cout << pixel << " ";
 }
-
-
+cout << endl;
+cout << pixel << "printed vertically" << endl;
+for (int i = 0; i < 10; i++){
+    cout << pixel << endl;
+}
+cout << endl << "Print a box " << W << " by " << H << endl;
+// nested loop
+for (int h=0; h<H; h++){
+    for (int w =0; w<W; w++){
+        cout << pixel << " ";
+    }
+    cout << endl;
+}
 
     return 0;
 }
