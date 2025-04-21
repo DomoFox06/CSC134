6cf9e265815c3a8ca7d1f90afd86c3d6304bfb29
DomoFox06 <yarnalld0401@student.faytechcc.edu>
2025-04-21 15:03:38 +0000

    save

--- Module_6/M6Lab1.cpp
+++ Module_6/M6Lab1.cpp
@@ -26,9 +26,28 @@ for (int shell : magazine){
     }
     cout << "Next shell: " << shellname << endl;
 }
+// Using a Vector Ex:2
+// size() tells you how many are in the vector
+// .push_back(0 removes an item from the end of vector
+vector<int> mag;
 
+mag.push_back(RED);
+mag.push_back(BLACK);
+int num_shells = 2;
 
+int current_shell = mag.at(num_shells - 1);
+cout << "Next shell: " << current_shell << endl;
 
+mag.pop_back();
+
+num_shells--;
+
+current_shell = mag.at(num_shells - 1);
+cout << "Next shell: " << current_shell << endl;
+
+mag.pop_back();
+cout << "Mag contains " << mag.size() << " shells." << endl;
+cout << "It could hold up to " << mag.capacity() << " before resizing." << endl;
 
 
 
