fb2aa86d1d7d5f5caa41d23f480d4920e03f8938
DomoFox06 <yarnalld0401@student.faytechcc.edu>
2025-04-25 14:53:39 +0000

    savw

--- Module_7/Final_Project.cpp
+++ Module_7/Final_Project.cpp
@@ -8,11 +8,13 @@ Final Project
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <string>
 using namespace std;
 
 void Start();
 void Charecter();
 void Name();
+void GameStart();
 //The dice rolling random number
 int rollDice() {
     return (rand() % 10 + 1);
@@ -54,13 +56,13 @@ int main(){
     do{
      cout << "Please input s to start the game and choose your avatar: " ;
     cin >> choice;
-    } while (s != s); 
+    } while (s != choice); 
     if (s = s){
-        void Charectar();
+        Charecter();
     }
 }
 void Charecter(){
-cout << "Time to choose your charecter!" << endl;
+cout << endl << "Time to choose your charecter!" << endl;
 int p1, p1a, p2, p2a;
 char Fla = '🦩';
 char Fox = '🦊';
@@ -71,9 +73,11 @@ char P = '🐧';
 
 cout << "Player 1, please choose your charecter!";
 cout << " 1. " << Fla << " 2. " << Fox << " 3. " << L << " 4. " << BR << " 5. " << BA << " 6. " << P << endl;
-cout << "Input 1-6: " ;
 int p1a_C;
+do{
+cout << "Input 1-6: " ;
 cin >> p1a_C;
+} while (p1a_C > 6 || p1a_C < 0);
 if (p1a_C = 1){p1a = Fla;}
 if (p1a_C = 2){p1a = Fox;}
 if (p1a_C = 3){p1a = L;}
@@ -84,9 +88,11 @@ if (p1a_C = 6){p1a = P;}
 cout << "Good choice!" << endl;
 cout << "Player 2, please choose your charecter!";
 cout << " 1. " << Fla << " 2. " << Fox << " 3. " << L << " 4. " << BR << " 5. " << BA << " 6. " << P << endl;
-cout << "Input 1-6: ";
 int p2a_C;
+do{
+cout << "Input 1-6: ";
 cin >> p2a_C;
+} while (p2a_C > 6 || p2a < 0);
 if (p2a_C = 1){p2a = Fla;}
 if (p2a_C = 2){p2a = Fox;}
 if (p2a_C = 3){p2a = L;}
@@ -94,23 +100,36 @@ if (p2a_C = 4){p2a = BR;}
 if (p2a_C = 5){p2a = BA;}
 if (p2a_C = 6){p2a = P;}
 cout << "Good choice!" << endl;
-void Name();
+ Name();
 
 }
 void Name(){
-    cout << "Now you must choose your names!" << endl;
-    cout << "Player 1, please input your name: ";
+    cout << endl << "Now you must choose your names!" << endl;
+    string p1N;
     string p1n;
-    cin >> p1n;
-    cout << "Welcome " << p1n << "! That is a great name!"
+    do{
+    cout << "Player 1, please input your name: ";
+    cin >> p1N;
+    std::getline(std::cin, p1N);  // Read input including spaces
+
+    if (p1N.find(' ')!= std::string::npos) {
+        std::cout << endl << endl << "Space detected in the string." << std::endl;
+        cout << "Sorry, no spaces allowed in name. Please try again" << endl << endl;
+    }
+    else{
+  //  p1n = p1N;
+    }
+    } while (p1N.find(' ')!= std::string::npos);
+   
+    cout << "Welcome " << p1N << "! That is a great name!" << endl;
     cout << "Player 2, please input your name: ";
     string p2n;
     cin >> p2n;
-    cout << "Welcome " << p2n << "! That is also a great name!";
+    cout << "Welcome " << p2n << "! That is also a great name!" << endl;
     cout << "Now that names and charecters are in order, let us begin!";
-    void GameStart();
+     GameStart();
 
 }
 void GameStart(){
-
+cout << endl << "Now I will explain how the game is played." << endl;
 }
